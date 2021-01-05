78654f0aa18bd7b0ec94750e3c4a98598dcd910d
nabilhayet <nabil4457@yahoo.com>
2021-01-05 18:40:47 -0500

    added migration

---
+++ db/migrate/20210105231622_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.integer :pages
+      t.integer :chapters
+      t.references :author, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
