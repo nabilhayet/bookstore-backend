21a27b44b3f585a8e7ae7b1031e195a0422d9db2
nabilhayet <nabil4457@yahoo.com>
2021-01-14 19:56:28 -0500

    added validation

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,11 @@
 class Author < ApplicationRecord
     has_many :books 
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :age, presence: true
+    validates :contact, presence: true
+
+    validates :age, numericality: { only_integer: true }
+    validates :contact, numericality: { only_integer: true }
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,11 @@
 class Book < ApplicationRecord
   belongs_to :author
+
+  validates :title, presence: true
+  validates :pages, presence: true
+  validates :chapters, presence: true
+  validates :author_id, presence: true
+
+  validates :pages, numericality: { only_integer: true }
+  validates :chapters, numericality: { only_integer: true }
 end
